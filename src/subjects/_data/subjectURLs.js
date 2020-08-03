e4d8d5dacf0c72ec1d6301347586211067e0f720
Jim O'Donnell <jim@zooniverse.org>
2020-08-03 10:39:26 +0100

    Batch subject pagination Break up subject URLs into 10 batches. Use separate pagination templates for each of those batches.

--- src/subjects/_data/subjectURLs.js
+++ src/subjects/_data/subjectURLs.js
@@ -2,5 +2,11 @@ const awaitSubjectURLs = require('../../helpers/subjectURLs');
 
 module.exports = async function () {
   const subjectURLs = await awaitSubjectURLs;
-  return subjectURLs;
+  const batchedURLs = [];
+  for (let i = 0; i < 10; i++) {
+    const start = i * 28000;
+    const finish = start + 28000;
+    batchedURLs.push(subjectURLs.slice(start, finish))
+  }
+  return batchedURLs;
 }
